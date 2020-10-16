cad5186c44ab04ae3c6b53938feabcfa694a75e5
Jack Linton <jacklinton@mac.com>
2020-10-16 11:41:20 -0400

    Fix tsconfig and add some comments to utils

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,6 +1,11 @@
 import jsforce from 'jsforce'
 import { oauth2 } from './initForce'
 
+/**
+ * create new connection to Salesforce API with session
+ * @param req
+ * @returns {jsforce.Connection}
+ */
 export function sessionConnectObj(req) {
   return new jsforce.Connection({
     oauth2: { oauth2 },
@@ -9,14 +14,26 @@ export function sessionConnectObj(req) {
   })
 }
 
+/**
+ * create new connection to Salesforce API without session
+ * @returns {jsforce.Connection}
+ */
 export function newConnectObj() {
   return new jsforce.Connection({ oauth2 })
 }
 
+/**
+ * apply limit and offset to query
+ * @param query
+ * @param pageSize
+ * @param pageNum
+ * @returns {*}
+ */
 export function paginate(query, pageSize, pageNum) {
   if (!pageSize) {
     return query
   }
+
   return query.limit(pageSize).offset(pageSize * (pageNum - 1))
 }
 
