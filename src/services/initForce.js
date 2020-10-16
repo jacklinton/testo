e01269d56dac4e1bba14e1c0eab65c2fd70c7472
Jack Linton <jacklinton@mac.com>
2020-10-15 23:54:00 -0400

    Lint force init

--- src/services/initForce.js
+++ src/services/initForce.js
@@ -1,8 +1,8 @@
 import jsforce from 'jsforce'
 
 export const oauth2 = new jsforce.OAuth2({
-  loginUrl : process.env.LOGIN_URL,
-  clientId : process.env.CLIENT_ID,
-  clientSecret : process.env.CLIENT_SECRET,
-  redirectUri : 'https://localhost:8443/token'
+  loginUrl: process.env.LOGIN_URL,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: 'https://localhost:8443/token',
 })
