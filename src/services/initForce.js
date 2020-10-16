27d88220a33da25f45e4b6d34b6db916256d7422
Jack Linton <jacklinton@mac.com>
2020-10-16 08:24:04 -0400

    Add check for env variable settings on force init

--- src/services/initForce.js
+++ src/services/initForce.js
@@ -1,8 +1,21 @@
 import jsforce from 'jsforce'
+import logger from '@apis/node-logger'
+
+const loginUrl = process.env.LOGIN_URL
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
+const redirectUri = process.env.REDIRECT_URI
+
+if (!loginUrl || !clientId || !clientSecret || !redirectUri) {
+  const err = new Error(`One or more environment variables for OAuth is not defined.
+    Check settings and add all necessary values to continue using the app.`)
+  logger.error(err)
+  process.abort()
+}
 
 export const oauth2 = new jsforce.OAuth2({
-  loginUrl: process.env.LOGIN_URL,
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  redirectUri: 'https://localhost:8443/token',
+  loginUrl,
+  clientId,
+  clientSecret,
+  redirectUri,
 })
